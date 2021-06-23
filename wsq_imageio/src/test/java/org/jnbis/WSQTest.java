2bfafb549bf508d627657bfaa88cd90158bf4d51
Paulo Costa <me@paulo.costa.nom.br>
2021-06-22 23:47:42 -0300

    Convert to gradle

--- wsq_imageio/src/test/java/org/jnbis/WSQTest.java
+++ wsq_imageio/src/test/java/org/jnbis/WSQTest.java
@@ -1,13 +1,10 @@
-package org.jnbis.test;
+package org.jnbis;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -19,11 +16,6 @@ import org.jnbis.WSQDecoder;
 import org.jnbis.WSQEncoder;
 
 public class WSQTest {
-
-	private static final String WSQ_FILE_IN = "t:/sample_image.wsq";
-//	private static final String WSQ_FILE_IN = "t:/FingerImage_1_1_right_index.wsq";
-	private static final String WSQ_FILE_OUT = "t:/sample_image_out5.wsq";
-
 	public WSQTest() {
 		testDecode();
 		testEncode();
@@ -31,8 +23,7 @@ public class WSQTest {
 
 	public void testDecode() {
 		try {
-			File file = new File(WSQ_FILE_IN);
-			Bitmap bitmap = WSQDecoder.decode(new FileInputStream(file));
+			Bitmap bitmap = WSQDecoder.decode(openExampleWsqFile());
 			System.out.println("bitmap = " + bitmap);
 			BufferedImage image = convert(bitmap);
 			showImage(image);
@@ -43,20 +34,17 @@ public class WSQTest {
 
 	public void testEncode() {
 		try {
-			File file = new File(WSQ_FILE_IN);
-			System.out.println("DEBUG: WSQ_FILE_IN length = " + file.length());
-			Bitmap bitmap = WSQDecoder.decode(new FileInputStream(file));
-			OutputStream outputStream = new FileOutputStream(WSQ_FILE_OUT);
+			Bitmap bitmap = WSQDecoder.decode(openExampleWsqFile());
+
+			ByteArrayOutputStream encodedOutputdataBuffer = new ByteArrayOutputStream();
 			float bitrate = 0.75f;
 //			int depth = 24; /* set it in bitmap */
 //			int ppi = 500; /* set it in bitmap */
 			String commentText = "";
-			WSQEncoder.encode(outputStream, bitmap, bitrate, commentText);
-			outputStream.close();
+			WSQEncoder.encode(encodedOutputdataBuffer, bitmap, bitrate, commentText);
+			encodedOutputdataBuffer.close();
 			
-			file = new File(WSQ_FILE_OUT);
-			System.out.println("DEBUG: WSQ_FILE_OUT length = " + file.length());
-			bitmap = WSQDecoder.decode(new FileInputStream(file));
+			bitmap = WSQDecoder.decode(new ByteArrayInputStream(encodedOutputdataBuffer.toByteArray()));
 			System.out.println("bitmap = " + bitmap);
 			BufferedImage image = convert(bitmap);
 			showImage(image);
@@ -84,6 +72,10 @@ public class WSQTest {
 		WritableRaster raster = image.getRaster();
 		raster.setDataElements(0, 0, width, height, data);
 		return image;
+	}
+
+	private static InputStream openExampleWsqFile() {
+		return WSQTest.class.getResourceAsStream("sample_image.wsq");
 	}
 
 	public static void main(String[] arg) {
